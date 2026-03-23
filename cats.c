93191735a6cb34d091b37b0f3e6f3f9de473f19b
liu <lelouch4217@gmail.com>
2026-03-23 18:06:08 +0100

    small touches

--- cats.c
+++ cats.c
@@ -2,19 +2,16 @@
 
 int main(){
 	int random_num;
-	printf("Gimme a random number :3");
+	printf("Gimme a random number :3\n");
 	scanf("%d",&random_num);
 	if (random_num<0)
 	{
-		printf("You are in debt! bring me %d cat the -w-",-1*random_num);
+		printf("You are in debt! bring us %d cat the -w-",-1*random_num);
 	}else if (random_num>0)
 	{
-		printf("Lucky you! You won %d cat from us~ >v<",random_num);
+		printf("Lucky you! You won %d cat from us~ >v<\nweee~",random_num);
 	}else{
-		printf("How unlucky... you did this purpusly didnt you");
+		printf("How unfortunate... you did this purposly didnt you -_-");
 	}
-	
-	
-	printf("Heyy cats!!");
 	return 0;
 }
